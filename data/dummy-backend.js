a42d02d26eee4fcf484d2ea76500b9e760ca6c55
amirabas <abasaarafi@outlook.com>
2024-04-25 04:21:54 +0330

    create datanavlink

--- data/dummy-backend.js
+++ data/dummy-backend.js
@@ -1,6 +1,15 @@
-export const dataProduct =[
-        {id : "p1",title: "labtop"},
-        {id : "p2",title: "phone"},
-        {id : "p3",title: "cool pad"},
-        {id : "p4",title: "notbook"}
-    ]
+export const dataProduct = [
+    { id: "p1", title: "labtop" },
+    { id: "p2", title: "phone" },
+    { id: "p3", title: "cool pad" },
+    { id: "p4", title: "notbook" }
+]
+
+
+export const navlink = [
+    "home", "info", "link", "pages"
+]
+
+export function dataNavlink () {
+    return {DATA : navlink}
+}
