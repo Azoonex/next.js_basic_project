e85ea90778a9e786298a06cbe3da897021ff533a
amirabas <amirjabic@gmail.com>
2024-04-28 11:30:23 +0330

    remove starting project for firebase

--- pages/products/_app.js
+++
@@ -1,7 +0,0 @@
-
-export default function App({ Component, pageProps }) {
-    return <main>
-        <h1>hello world</h1>
-        <Component {...pageProps} />
-    </main>;
-}
--- pages/products/index.js
+++
@@ -1,7 +0,0 @@
-
-
-export default function index() {
-  return (
-    <h3 className="text-center uppercase mt-20 text-5xl">main create products</h3>
-  )
-}
